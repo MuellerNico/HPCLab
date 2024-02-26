b532087d4c7652e9f4cc1ae562d815f70cf7bb18
Nico <muellernico@outlook.com>
2024-02-26 16:12:24 +0100

    2 node script. part 1 done.

--- project1/helloworld.cpp
+++ project1/helloworld.cpp
@@ -8,7 +8,7 @@ int main() {
 	char username[LOGIN_NAME_MAX];
 	gethostname(hostname, HOST_NAME_MAX);
 	getlogin_r(username, LOGIN_NAME_MAX);
-	std::cout << "Hello, " << username << ", the host name of your machine is: " << hostname << std::endl;
+	std::cout << "hostname : " << hostname << std::endl;
 }
 
 
