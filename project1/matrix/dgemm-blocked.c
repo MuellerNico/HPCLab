9a63d55f4f15bf94fa238e4d2a4c43c7218dea1c
Nico <muellernico@outlook.com>
2024-03-10 16:37:46 +0100

    added blocking

--- project1/matrix/dgemm-blocked.c
+++ project1/matrix/dgemm-blocked.c
@@ -7,13 +7,33 @@ const char* dgemm_desc = "Blocked dgemm.";
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values.
  */
+
 void square_dgemm(int n, double* A, double* B, double* C) {
-  // TODO: Implement the blocking optimization
-  //       (The following is a placeholder naive three-loop dgemm)
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      for(int k = 0; k < n; ++k) {
-	C[i+j*n] += A[i+k*n] * B[k+j*n];
+  int s = 36; // this value is specific for the 32 kB L1d cache of the AMD EPYC 7763 processor
+  if (n < s) {
+    s = n;
+  }
+  for (int i = 0; i < n; i += s) {
+    for (int j = 0; j < n; j += s) {
+      for (int k = 0; k < n; k += s) {
+        // block loop
+        // adjust block size when close to the edge of the matrix
+        if (i + s > n) {
+          s = n - i;
+        }
+        if (j + s > n) {
+          s = n - j;
+        }
+        if (k + s > n) {
+          s = n - k;
+        }
+        for (int ii = i; ii < i + s; ++ii) {
+          for (int jj = j; jj < j + s; ++jj) {
+            for (int kk = k; kk < k + s; ++kk) {
+              C[ii + jj * n] += A[ii + kk * n] * B[kk + jj * n];
+            }
+          }
+        }
       }
     }
   }
