7840d808d05369dd497028052c9935ea5875a1f5
Nico Müller <muellernico@outlook.com>
2024-03-11 20:42:44 +0100

    submission

--- project1/matrix/dgemm-blocked.c
+++ project1/matrix/dgemm-blocked.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 const char* dgemm_desc = "Blocked dgemm.";
 
 /* This routine performs a dgemm operation
@@ -8,34 +9,41 @@ const char* dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values.
  */
 
-// todo: use pragmas, vectorization
+// using padding (this is the version documented in the report)
 void square_dgemm(int n, double* A, double* B, double* C) {
-  int s = 36; // this value is specific for the 32 kB L1d cache of the AMD EPYC 7763 processor
-  if (n < s) {
-    s = n;
-  }
-  for (int i = 0; i < n; i += s) {
-    for (int j = 0; j < n; j += s) {
-      for (int k = 0; k < n; k += s) {
-        // block loop
-        // adjust block size when close to the edge of the matrix
-        if (i + s > n) {
-          s = n - i;
+    int s = 36; // block size
+    int n_pad = ((n + s - 1) / s) * s; // padded n
+
+    // use padded matrices
+    double* A_pad = (double*)calloc(n_pad * n_pad, sizeof(double));
+    double* B_pad = (double*)calloc(n_pad * n_pad, sizeof(double));
+
+    // Copy data
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A_pad[j * n_pad + i] = A[j * n + i];
+            B_pad[j * n_pad + i] = B[j * n + i];
         }
-        if (j + s > n) {
-          s = n - j;
-        }
-        if (k + s > n) {
-          s = n - k;
-        }
-        for (int ii = i; ii < i + s; ++ii) {
-          for (int jj = j; jj < j + s; ++jj) {
-            for (int kk = k; kk < k + s; ++kk) {
-              C[ii + jj * n] += A[ii + kk * n] * B[kk + jj * n];
-            }
-          }
-        }
-      }
     }
-  }
+
+    // outer loop
+    for (int i = 0; i < n_pad; i += s) {
+        for (int j = 0; j < n_pad; j += s) {
+            for (int k = 0; k < n_pad; k += s) {
+                // block loop
+                for (int ii = 0; ii < s; ii++) {
+                    for (int kk = 0; kk < s; kk++) {
+                      // precompute and load A_ik
+                      double A_ik = A_pad[(k + kk) * n_pad + i + ii]; 
+                      for (int jj = 0; jj < s; jj++) {
+                          C[(j + jj) * n + i + ii] += A_ik * B_pad[(j + jj) * n_pad + k + kk];
+                      }
+                    }
+                }
+            }
+        }
+    }
+
+    free(A_pad);
+    free(B_pad);
 }
